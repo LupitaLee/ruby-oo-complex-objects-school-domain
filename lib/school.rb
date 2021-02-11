37059991d87171856a4e77ab653fb80f8e385223
Lupita <lupitarivera8899@gmail.com>
2021-02-10 23:27:55 -0600

    commit tree baa0f87dc3e0bccc9fcb495a58112be0e24a27cd

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
 # code here!
+require "pry"
+class School
+
+    def initialize(roster)
+        roster = {}
+        @roster = roster
+        @length = length
+    end
+
+    def roster
+        @roster
+    end
+
+    def length
+        @length
+        end
+
+        def add_student(student_name,grade)
+         binding.pry
+    # @roster ={10 =>["AC Slater"]}
+    if @roster[grade]= []
+    @roster[grade]<<student_name
+end
+end
+end
